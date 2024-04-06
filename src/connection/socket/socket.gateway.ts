39e08dc29ec92bcc84fe0592e37f0988b21b257a
boss <oncelemon02@gmail.com>
2024-04-06 15:21:19 +0700

    ..

--- src/connection/socket/socket.gateway.ts
+++ src/connection/socket/socket.gateway.ts
@@ -20,7 +20,6 @@ import { ForbiddenException } from '@nestjs/common';
       'http://localhost:3000',
       'http://localhost:3001',
       'http://localhost:3010',
-      'http://202.151.182.145',
       'http://202.151.182.145:3000',
     ],
   },
